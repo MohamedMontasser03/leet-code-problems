d291e82a31e3a29d9f36b5b616108a3c34fe5e04
Mohamed Montasser <90785835+MohamedMontasser03@users.noreply.github.com>
2022-06-24 12:36:12 +0200

    Time: 770 ms (5.08%), Space: 93.1 MB (8.30%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function isPalindrome(l: ListNode | null): boolean {
+    let midPoint = l, endPoint = l;
+    console.log(l);
+    while(endPoint !== null && endPoint.next !== null){
+        endPoint = endPoint.next.next;
+        midPoint = midPoint.next;
+    }
+    let temp1 = midPoint, temp2 = midPoint.next;
+    if(temp2 === null) return l.val === midPoint.val;
+    while(temp2 !== null && temp1 !== null){
+        midPoint = temp2;
+        temp2 = temp2.next;
+        midPoint.next = temp1;
+        temp1 = midPoint;
+    }
+    while(midPoint.next !== l && midPoint !== l){
+        if(midPoint.val !== l.val)
+            return false;
+        midPoint = midPoint.next;
+        l = l.next;
+    }
+    return true;
+};
